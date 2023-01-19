6857e34550e3b44d1a2cb502bb811db02937e927
Gustavo Corrêa <110264371+gustavomcss@users.noreply.github.com>
2023-01-19 20:45:11 -0300

    Refiz o Site

---
+++ script.js
@@ -0,0 +1,13 @@
+function clickMenu() {
+    if (itens.style.display == 'block') {
+        itens.style.display = 'none'
+    } else {
+        itens.style.display = 'block'
+    }
+}
+
+function mudouTamanho() {
+    if (window.innerWidth >= 600) {
+        itens.style.display = 'none'
+    }
+}
